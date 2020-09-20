75e7bcf25c04ebb1d0502401e37790e361a9a2cd
Ojas Sethi <sethi.ojas@gmail.com>
2020-09-20 17:57:02 +0530

    show road

--- screen.py
+++ screen.py
@@ -5,12 +5,17 @@ class Screen:
     _NIGHT_SKY_COLOR = (11, 16, 38)
     _MOON_IMAGE = pygame.image.load('images/moon.png')
     _MOON_POSITION = (720, 10)
+
+    _ROAD_IMAGE = pygame.image.load('images/road.png')
+
     screen = pygame.display.set_mode(_WINDOW)
 
     @classmethod
     def load(cls):
         cls.screen.fill(cls._NIGHT_SKY_COLOR)
         cls.screen.blit(cls._MOON_IMAGE, cls._MOON_POSITION)
+        for x in range(0, 800, 64):
+            cls.screen.blit(cls._ROAD_IMAGE, (x, 536))
 
     @classmethod
     def window_x(cls):
