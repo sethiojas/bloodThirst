7a02c90dcad6fd9ca00f66d73e2bdb6b96614ffc
Ojas Sethi <sethi.ojas@gmail.com>
2020-09-20 18:43:38 +0530

    randomly select direction in which pedestrian moves

--- pedestrian.py
+++ pedestrian.py
@@ -1,13 +1,18 @@
 import pygame
 from screen import Screen
 from vampire import Vampire
+from random import randint
 
 class Pedestrian:
+
     def __init__(self):
+        rand_number = randint(0, 1)
+        x_pos = [0, Screen.window_x()]
+        pos_change = [0.2, -0.2]
         self._PEDESTRIAN_IMAGE = pygame.image.load('images/woman.png')
-        self._pedestrian_position_x = 0
+        self._pedestrian_position_x = x_pos[rand_number]
         self._pedestrian_position_y = 490
-        self._position_change = 0.2
+        self._position_change = pos_change[rand_number]
 
     def move(self):
         self._pedestrian_position_x += self._position_change
