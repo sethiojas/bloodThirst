fa75155fd2194453f797e425532b4f89bd408fe3
Ojas Sethi <sethi.ojas@gmail.com>
2020-09-20 20:58:28 +0530

    on left start from -64 instead of 0

--- pedestrian.py
+++ pedestrian.py
@@ -7,7 +7,7 @@ class Pedestrian:
 
     def __init__(self):
         rand_number = randint(0, 1)
-        x_pos = [0, Screen.window_x()]
+        x_pos = [-64, Screen.window_x()]
         pos_change = [0.2, -0.2]
         self._PEDESTRIAN_IMAGE = pygame.image.load('images/woman.png')
         self._pedestrian_position_x = x_pos[rand_number]
@@ -18,7 +18,7 @@ class Pedestrian:
         self._pedestrian_position_x += self._position_change
         Screen.screen.blit(self._PEDESTRIAN_IMAGE, (self._pedestrian_position_x, self._pedestrian_position_y))
 
-        if (self._pedestrian_position_x < 0 or
+        if (self._pedestrian_position_x < -64 or
             self._pedestrian_position_x > 800):
             return True
         return False
