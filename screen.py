bd6ae035c010a6f65800cb9ccf959b1372f118ed
Ojas Sethi <sethi.ojas@gmail.com>
2020-09-21 12:52:02 +0530

    dumpsters added for hiding

--- screen.py
+++ screen.py
@@ -7,6 +7,10 @@ class Screen:
     _MOON_POSITION = (720, 10)
 
     _ROAD_IMAGE = pygame.image.load('images/road.png')
+    _ROAD_Y_COORDINATE = _WINDOW[1] -64
+    
+    _DUMPSTER_IMAGE = pygame.image.load('images/dumpster.png')
+    _DUMPSTER_Y_COORDINATE = _ROAD_Y_COORDINATE - 64
 
     screen = pygame.display.set_mode(_WINDOW)
 
@@ -15,7 +19,10 @@ class Screen:
         cls.screen.fill(cls._NIGHT_SKY_COLOR)
         cls.screen.blit(cls._MOON_IMAGE, cls._MOON_POSITION)
         for x in range(0, 800, 64):
-            cls.screen.blit(cls._ROAD_IMAGE, (x, 536))
+            cls.screen.blit(cls._ROAD_IMAGE, (x, cls._ROAD_Y_COORDINATE))
+        cls.screen.blit(cls._DUMPSTER_IMAGE, (170, cls._DUMPSTER_Y_COORDINATE))
+        cls.screen.blit(cls._DUMPSTER_IMAGE, (570, cls._DUMPSTER_Y_COORDINATE))
+
 
     @classmethod
     def window_x(cls):
